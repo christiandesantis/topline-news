a0bc543d0b226f8cd0e315aa276bec623f817b29
christiandesantis <christian.constantino98@gmail.com>
2024-12-29 02:29:44 -0400

    :sparkles: implement new components to dashboard page component

--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -1,7 +1,22 @@
+import ArticleCard from "@/Components/ArticleCard";
+import Pagination from "@/Components/Pagination";
+import SearchInput from "@/Components/SearchInput";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
-import { Head } from "@inertiajs/react";
+import { Article, Paginated } from "@/types";
+import { Head, router, usePage } from "@inertiajs/react";
+import { useState } from "react";
 
-const Dashboard = () => {
+const Dashboard = ({
+    articlesPaginated,
+}: {
+    articlesPaginated: Paginated<Article>;
+}) => {
+    const { url } = usePage();
+    const searchQuery = new URLSearchParams(window.location.search).get(
+        "search",
+    ) as string;
+    const [search, setSearch] = useState(searchQuery);
+
     return (
         <AuthenticatedLayout
             header={
@@ -14,9 +29,26 @@ const Dashboard = () => {
 
             <div className="py-12">
                 <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
-                    <div className="overflow-hidden bg-base-100 shadow-sm sm:rounded-lg">
-                        <div className="p-6">You're logged in!</div>
+                    <SearchInput
+                        className="mb-6"
+                        defaultValue={search}
+                        disabled={!searchQuery && !search}
+                        onChange={(e) => setSearch(e.target.value)}
+                        onSubmit={() =>
+                            router.visit(url, {
+                                method: "get",
+                                data: { search, page: 1 },
+                            })
+                        }
+                    />
+
+                    <Pagination paginated={articlesPaginated} />
+                    <div className="grid grid-cols-1 gap-4 p-6 sm:grid-cols-2 md:grid-cols-3">
+                        {articlesPaginated.data.map((article) => (
+                            <ArticleCard key={article.id} article={article} />
+                        ))}
                     </div>
+                    <Pagination paginated={articlesPaginated} />
                 </div>
             </div>
         </AuthenticatedLayout>
