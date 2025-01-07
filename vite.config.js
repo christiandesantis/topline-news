3220caf538f5bf4fa23914e2ecb2012c6fe42d90
christiandesantis <christian.constantino98@gmail.com>
2025-01-07 18:16:39 -0400

    :rocket: add env, config and script for remote dev setup

--- vite.config.js
+++ vite.config.js
@@ -15,7 +15,7 @@ export default defineConfig({
         host: "0.0.0.0",
         port: 5173,
         hmr: {
-            host: "localserver",
+            host: process.env.VITE_HMR_HOST || "localhost",
         },
     },
 });
